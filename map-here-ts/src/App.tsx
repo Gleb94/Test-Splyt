9f91de1ee6196ed209d0670739c22728ea49d541
Gleb94 <58596253+Gleb94@users.noreply.github.com>
2021-12-09 17:16:03 +0300

    add

---
+++ map-here-ts/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Map from './component/Map/Map'
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <Map></Map>
+    </div>
+  );
+}
+export default App;
